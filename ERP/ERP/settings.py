a01f10ef2da5ea2fb892eea31a408cc39faddcf0
sinaihmels <sinaihmels@gmail.com>
2024-03-31 16:22:26 +0200

    Implement register app

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig', # setting up the dashboard app
-    'register.apps.RegisterConfig', # setting up the register app
+    'users.apps.UsersConfig' # setting up the users app
 ]
 
 MIDDLEWARE = [
--- ERP/dashboard/urls.py
+++ ERP/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "dashboard"
 urlpatterns = [
     path('inventory/', views.index, name='index'),
     path('', views.dashboard, name='dashboard'),
